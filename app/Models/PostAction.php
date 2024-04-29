b7a11449775645bf0d989817cf58ead764ad83ac
BOCO Israel <isboco1@gmail.com>
2024-04-29 19:32:39 +0100

    add post user action

--- app/Models/PostAction.php
+++ app/Models/PostAction.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PostAction extends Model
 {
     use HasFactory;
+    protected $fillable =
+    [
+        'post_id',
+        'action_id'
+    ];
+
+    public function post(): BelongsTo
+    {
+        return $this->belongsTo(Post::class);
+    }
+
+    public function action(): BelongsTo
+    {
+        return $this->belongsTo(Action::class);
+    }
 }
--- app/Models/PostActionUser.php
+++ app/Models/PostActionUser.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class PostActionUser extends Model
 {
     use HasFactory;
+    protected $fillable =
+    [
+        'post_action_id',
+        'user_id'
+    ];
+
+    public function post_action(): BelongsTo
+    {
+        return $this->belongsTo(PostAction::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
